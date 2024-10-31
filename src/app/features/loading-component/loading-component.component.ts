60700fef538edcede1b28904a0fc691592bbf281
RAfaah121 <rafaelsantos42234@gmail.com>
2024-10-31 20:09:51 -0300

    Implementação do componente de loading

--- src/app/features/loading-component/loading-component.component.ts
+++ src/app/features/loading-component/loading-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loading-component',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loading-component.component.css']
 })
 export class LoadingComponentComponent implements OnInit {
-
+  @Input() isLoading: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
