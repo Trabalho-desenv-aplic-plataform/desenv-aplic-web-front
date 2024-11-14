b635eda31e9fe9aebf0b75a0158a0efc9cf9f6af
RAfaah121 <rafaelsantos42234@gmail.com>
2024-11-14 19:33:24 -0300

    crud de contatos

--- src/app/features/contatos/contatos.module.ts
+++ src/app/features/contatos/contatos.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContatosRoutingModule } from './contatos-routing.module';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ContatosComponent } from './contatos.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContatosComponent
+  ],
   imports: [
     CommonModule,
-    ContatosRoutingModule
+    ContatosRoutingModule,
+    ContatosRoutingModule,
+    MatIconModule,
+    FormsModule,
+    ColorPickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ]
 })
 export class ContatosModule { }
